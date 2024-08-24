ecc8fa7fd7344d6d387c625b336902f564b5d3d0
Joshua <jdotaster@gmail.com>
2024-08-23 20:42:31 -0600

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: './src/app.js',
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+   devtool: 'inline-source-map',
+   devServer: {
+    static: './dist',
+    },
+    mode: 'development',
+   module: {
+     rules: [
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+         type: 'asset/resource',
+       },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+         type: 'asset/resource',
+       },
+     ],
+   },
+   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weathr',
+      template: './src/index.html'
+    }),
+  ],
+ };
